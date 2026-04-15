75ba5f84f2c94dbbaecd99606125e31aabd35a45
Zsombor Szende <zsombor.szende@axoflow.com>
2026-04-15 14:28:39 +0200

    fix(spec): Fix OpenAPI spec not updating on page reload.
    GH-24

--- src/build/command/build.ts
+++ src/build/command/build.ts
@@ -1,9 +1,10 @@
 import {build as viteBuild} from 'vite';
 import {PUBLIC_PATH} from '../../public-path';
-import {replaceClientConfigPath, isOpenApiSpecPlaceValidUtil} from '../../serve';
+import {writeSpecFile, isOpenApiSpecPlaceValidUtil} from '../../serve';
 import signale from 'signale';
 import {BuildOptionsModel} from '../models';
 import {join} from 'path';
+import {existsSync} from 'fs';
 import {viteSingleFile} from 'vite-plugin-singlefile';
 
 export const build = async (openApiSpecFilePathOrURL: string, options: BuildOptionsModel) => {
@@ -12,11 +13,17 @@ export const build = async (openApiSpecFilePathOrURL: string, options: BuildOpti
     return process.exit(1);
   }
 
-  await replaceClientConfigPath(options.config, openApiSpecFilePathOrURL);
+  await writeSpecFile(openApiSpecFilePathOrURL);
+  const userConfigFile = join(process.cwd(), options.config);
 
   await viteBuild({
     root: PUBLIC_PATH,
     plugins: options.multiFileBuild ? [] : [viteSingleFile()],
+    resolve: {
+      alias: {
+        '@user-config': existsSync(userConfigFile) ? userConfigFile : join(PUBLIC_PATH, 'swagger-ui.config'),
+      },
+    },
     build: {
       outDir: join(process.cwd(), options.output),
     },
--- src/serve/models/serve-options.model.ts
+++ src/serve/models/serve-options.model.ts
@@ -7,8 +7,25 @@ export type ServerConfig = InlineConfig;
 export type ClientConfig = SwaggerUIConfigOptions;
 
 export interface ServeOptionsModel {
+  /**
+   * Port number for the dev server to listen on.
+   */
   port: number;
+
+  /**
+   * Hostname for the dev server (e.g. "localhost").
+   */
   domain: string;
+
+  /**
+   * Path to the Swagger UI client configuration file.
+   * (SwaggerUIOptions overrides)
+   */
   config: string;
+
+  /**
+   * Path to the Vite server configuration file.
+   * (InlineConfig overrides)
+   */
   serverConfig: string;
 }
